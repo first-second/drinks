9033e2cc7afdcf9858ac86c02b0ec0eead63188b
first-second <firstsecondis72@gmail.com>
2023-07-18 23:12:40 +0530

    pandas dataframe in consume.py

--- drinks/consume.py
+++ drinks/consume.py
@@ -1,12 +1,21 @@
 from getpass import getpass
 import requests
+import pandas as pd
 url = 'http://127.0.0.1:8000/api-token-auth/'
 username=input("enter username : ")
 password=getpass("enter password : ")
 data = {'username': username, 'password': password}
 response = requests.post(url, data=data)
 response_data=response.json()
+print(response_data)
+print("+++++++++++++++++++++++++++++++++++")
 token=response_data['token']
 headers = {'Authorization': 'Token ' + token}
 response=requests.get('http://127.0.0.1:8000/drinks/',headers=headers)
+#response_test = requests.get('http://127.0.0.1:8000/drinks/')
+#print(response_test.json())
 print(response.json())
+data = response.json()
+print("+++++++++++++++++++++++++++++++++++")
+df = pd.DataFrame(data)
+print(df.to_string(index=False))
